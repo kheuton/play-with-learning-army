dc0cdd648ec1f85b28e94d77f811a9f3c50a10b5
Kyle Heuton <kheuton@gmail.com>
2025-02-15 15:40:20 -0500

    GetTestResults now displays numbers

--- BestModels.md
+++ BestModels.md
@@ -3,13 +3,8 @@
 ## BERT Finetune
 20 Test students
 Equivalent final val_auprc. Chose largest alpha+beta arbitrarily. *=choice
-- test20_lr1e-06_alpha0.01_beta0.1*
-- test20_lr1e-06_alpha1e-05_beta0.0001
+- test20_lr5e-07_alpha0.01_beta0.01
 
 15 Test students
 Equivalent final val_auprc. Chose largest alpha+beta arbitrarily. *=choice
-- test15_lr1e-06_alpha0.1_beta0.01*
-- test15_lr1e-06_alpha0_beta0.0001
-- test15_lr1e-06_alpha0.001_beta0
-- test15_lr1e-06_alpha0.001_beta0.1
-- test15_lr1e-06_alpha1e-05_beta0.01
+- test15_lr5e-07_alpha0.001_beta0.1
--- src/submit_jobs.py
+++ src/submit_jobs.py
@@ -12,16 +12,17 @@ def create_slurm_script(problem_config, hyper_config):
 #SBATCH -o /cluster/tufts/hugheslab/kheuto01/slurmlog/out/log_%j.out       # Write stdout to file named log_JOBIDNUM.out in log dir
 #SBATCH -e /cluster/tufts/hugheslab/kheuto01/slurmlog/err/log_%j.err       # Write stderr to file named log_JOBIDNUM.err in log dir
 #SBATCH --time=6:00:00
-#SBATCH --mem=16GB
-#SBATCH --partition=batch
+#SBATCH --mem=32GB
+#SBATCH --partition=hugheslab
 #SBATCH -n 4
 #SBATCH -N 1
+#SBATCH --gres=gpu:1
 #SBATCH --export=ALL
 
 export PYTHONPATH=$PYTHONPATH:/cluster/home/kheuto01/code/play-with-learning-army/src
 
 # Run the training script
-python src/model_training.py --problem_config {problem_config} --hyper_config {hyper_config}
+python src/model_training_frozen.py --problem_config {problem_config} --hyper_config {hyper_config}
 """
 
 def submit_job(problem_config, hyper_config):

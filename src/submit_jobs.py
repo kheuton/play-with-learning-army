9f7501822d4b2e6eff136228359a88306e61d175
Kyle Heuton <kheuton@gmail.com>
2025-02-16 14:06:17 -0500

    Fix GetTestResults.ipynb for non finetune models

--- src/submit_jobs.py
+++ src/submit_jobs.py
@@ -11,12 +11,11 @@ def create_slurm_script(problem_config, hyper_config):
 #SBATCH --job-name=finetune
 #SBATCH -o /cluster/tufts/hugheslab/kheuto01/slurmlog/out/log_%j.out       # Write stdout to file named log_JOBIDNUM.out in log dir
 #SBATCH -e /cluster/tufts/hugheslab/kheuto01/slurmlog/err/log_%j.err       # Write stderr to file named log_JOBIDNUM.err in log dir
-#SBATCH --time=6:00:00
-#SBATCH --mem=32GB
-#SBATCH --partition=hugheslab
+#SBATCH --time=3:00:00
+#SBATCH --mem=16GB
+#SBATCH --partition=batch
 #SBATCH -n 4
 #SBATCH -N 1
-#SBATCH --gres=gpu:1
 #SBATCH --export=ALL
 
 export PYTHONPATH=$PYTHONPATH:/cluster/home/kheuto01/code/play-with-learning-army/src
